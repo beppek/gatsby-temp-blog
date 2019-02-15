f2a2542a9f4b8c45708ebabf30e7b47eb616b77c
Beppe Karlsson <beppe@live.se>
2019-02-15 20:37:49 +1100

    Update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,71 +1,54 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import image from '../img/code.jpg'
 
 export default () => (
   <Layout>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dictum
-      nibh ante, nec dapibus orci pharetra sit amet. Donec semper erat id sem
-      condimentum, ac imperdiet tellus eleifend. Etiam a varius ex. Quisque
-      finibus interdum elit vel eleifend. Proin in nisi id nisl eleifend
-      condimentum. Cras semper at sem a bibendum. Vivamus lorem ante, tempor
-      quis est id, fringilla molestie velit. Phasellus imperdiet lectus at massa
-      rutrum, ut laoreet dui tempor. Pellentesque rhoncus consectetur felis, eu
-      pharetra felis. Fusce ex dui, euismod et porta accumsan, malesuada sit
-      amet tellus. Nunc mollis mi in sagittis convallis. Nunc posuere, leo ac
-      varius ornare, arcu eros pellentesque felis, in auctor urna ligula sit
-      amet lorem. Aenean rutrum ante magna, non ornare quam dictum vitae. Fusce
-      in nibh suscipit, malesuada purus eget, congue justo. Aliquam erat
-      volutpat.
+    <h1>About Me</h1>
+    <img src={image} alt="TypeScript code on a computer screen" />
+    <p
+      style={{
+        fontSize: '14px'
+      }}
+    >
+      <strong>Name:</strong> Max Karlsson <br />
+      <strong>Location:</strong> Melbourne <br />
+      <strong>Favourite (programming) languages:</strong> JavaScript,
+      TypeScript, Python
+      <br />
+      <strong>Favourite DBMS:</strong> Neo4j & MongoDB. <br />
+      <strong>Education:</strong> Bachelor's degree from Linnaeus University in
+      Sweden. <br />
     </p>
-
+    <h4>My story</h4>
     <p>
-      Etiam orci ex, iaculis non molestie in, consectetur non nulla. Duis ornare
-      urna ac risus blandit sollicitudin ac in turpis. Aenean laoreet, mi id
-      mollis vulputate, eros elit maximus ligula, in faucibus ligula libero eu
-      libero. Etiam at interdum massa. Suspendisse malesuada volutpat faucibus.
-      Donec vulputate sodales purus, finibus posuere lacus varius id. Phasellus
-      id sollicitudin eros. Quisque eu rhoncus ligula. Aenean auctor risus non
-      turpis ornare convallis. Morbi dolor mauris, aliquam ac imperdiet at,
-      volutpat vehicula elit. Etiam auctor dolor in neque facilisis bibendum.
-      Mauris sit amet dictum magna.
+      I'm a Swedish-born full stack developer who spent the better part of my
+      twenties travelling around, living and working in 4 different countries;
+      including Canada, Sweden, Norway and Australia.
     </p>
-
+    <h4>My passions</h4>
     <p>
-      Curabitur quis laoreet arcu. Integer a posuere risus, et suscipit neque.
-      Proin quis congue justo. Aenean rhoncus facilisis nibh ac iaculis. Proin
-      et bibendum ex. Sed id massa nunc. Aenean laoreet ligula nec accumsan
-      vehicula. Nam aliquet nisl nulla, at elementum orci pellentesque a. Nunc
-      quis hendrerit magna. Lorem ipsum dolor sit amet, consectetur adipiscing
-      elit. Ut finibus, orci at sollicitudin porta, tortor magna tristique nisl,
-      a fermentum erat mi id est.
+      I truly love JavaScript, even in spite of all its kinks and quirks, or
+      perhaps due to them. There are just so many things you can do with the
+      language: web apps, native apps, APIs, services, scripts, IoT, AI... the
+      list is long. With the addition of TypeScript you can also get typesafety
+      and improve the developer experience.
     </p>
-
     <p>
-      Vivamus quis ligula tempor, tincidunt massa at, convallis sem. Morbi sit
-      amet aliquam libero. Class aptent taciti sociosqu ad litora torquent per
-      conubia nostra, per inceptos himenaeos. Interdum et malesuada fames ac
-      ante ipsum primis in faucibus. Fusce orci erat, porttitor et magna eget,
-      condimentum molestie nunc. Interdum et malesuada fames ac ante ipsum
-      primis in faucibus. Phasellus at velit tempor, venenatis metus sit amet,
-      sagittis risus. Duis non auctor nunc. Morbi eu mi tellus. Donec neque ex,
-      aliquet a orci et, eleifend consequat ipsum. Nam ut commodo risus. Fusce
-      vitae vehicula nunc, vel viverra neque.
+      I'm also a big science fiction nerd, especially Star Trek and similarly
+      inspired stories. To me, exploring the unknown is what intrigues me,
+      perhaps unsurprising given my history of travelling the world. I believe
+      that science fiction has an important role to play in the world of today,
+      by either warning of things that could happen or inspiring us with the
+      wonders our future could hold in store if we only make it so.
     </p>
-
+    <h4>My mission</h4>
     <p>
-      Nulla ut ante ante. Aenean scelerisque et quam fermentum varius. Nullam
-      suscipit risus eu nibh tincidunt vulputate. Mauris eget mi sit amet nunc
-      semper malesuada. Etiam non convallis justo. Aliquam vulputate est ut ex
-      ornare efficitur. Suspendisse non mollis eros. Nam vehicula ultrices
-      tellus, nec pulvinar lectus ullamcorper non. Nulla facilisi. Nulla id
-      mauris maximus, laoreet purus ut, imperdiet lectus. Donec ac nulla laoreet
-      mi facilisis suscipit scelerisque eu turpis. Cras id arcu euismod,
-      sollicitudin massa vel, aliquet sem. Duis vel quam ultrices, fermentum
-      elit vel, elementum diam. Nunc ut nisi cursus, dignissim purus sed,
-      imperdiet leo. Duis nisi nibh, maximus ac nisi a, malesuada maximus nibh.
-      Nunc molestie porta vulputate.
+      As a programmer, I want to be a part of innovation and continue to build
+      the applications of tomorrow. As a writer I want to inspire people into
+      action, either to learn something new, or to make improvements to the
+      world we all share.
     </p>
   </Layout>
 )
