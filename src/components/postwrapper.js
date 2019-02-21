00f43cdc80b4d256ce7cbcf628f7dc376fe02a6f
Beppe Karlsson <beppe@live.se>
2019-02-22 06:42:28 +1100

    Fix width bug on mobile

--- src/components/postwrapper.js
+++ src/components/postwrapper.js
@@ -1,9 +1,9 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const PostWrapper = styled.div`
   margin: auto;
   padding: 30px;
-  // max-width: 720px;
+  max-width: 100%;
   font-size: 14px;
 
   @media (max-width: 414px) {
@@ -225,4 +225,4 @@ export const PostWrapper = styled.div`
   .gatsby-highlight .xml .cdata {
     opacity: 0.5;
   }
-`
+`;
